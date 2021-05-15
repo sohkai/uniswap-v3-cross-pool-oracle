7b76f3d34b85ee4feef6eaf5bc439598eab43233
Brett Sun <qisheng.brett.sun@gmail.com>
2021-05-15 13:17:10 +0100

    chore: add deployment dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require('@nomiclabs/hardhat-ethers')
+require("@nomiclabs/hardhat-etherscan");
+require('hardhat-local-networks-config-plugin')
 
 module.exports = {
   solidity: '0.7.6',
@@ -8,4 +10,7 @@ module.exports = {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 }
